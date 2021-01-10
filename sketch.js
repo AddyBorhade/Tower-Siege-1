1545d2cc31060443990282aac044c92192654307
AddyBorhade <73005303+AddyBorhade@users.noreply.github.com>
2021-01-09 21:26:32 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, 
+
+
+function preload() {
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    ground2 = new Ground(150, 305, 300, 170);
+
+    
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    ground.display() 
+    ground2.display()
+}
+
